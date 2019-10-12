30ec7858ca279f364dc0da12c851a103c603431d
rubinhotg <rubinhotg@live.com>
2019-10-11 22:30:42 -0300

    Subindo ProjetoFinal

--- Conexao.php
+++ Conexao.php
@@ -4,19 +4,19 @@ include ("config.php");
 class Conexao{
 	private static $instance;
 	public static function getInstance(){
-		if(!isset($this->$instance)){
+		if(!isset($self::$instance)){
 			try {
-				$this->$instance = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
-				$this->$instance->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-				$this->$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
-			//} catch (PDOException $e) {
-				//echo $e->getMessage();
-			//}
+				$self::$instance = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
+				$self::$instance->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+				$self::$instance->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_OBJ);
+			} catch (PDOException $e) {
+				echo $e->getMessage();
+			}
 		}
-		return $this->$instance;
+		return $self::$instance;
 	}
  	
 	public static function prepare($sql){
-		return $this->getInstance()->prepare($sql);
+		return $self::getInstance()->prepare($sql);
 	}
 }
